27d7a86ad14099d21868437dd5f9c777445ba475
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-24 21:47:36 +0200

    add

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@
 const randomNumbers = fiveRandomNumbers();
 //prendiamo dal dom l'ul
 const ul = document.getElementById("numbers-list");
+//prendiamo il form che contiene i campi dove inserire i numeri
+const answersForm = document.getElementById("answers-form");
 //prendiamo l'elemento dove inserire il countdown
 const countdown = document.getElementById("countdown");
 //inizializiamo all'esterno il countdown a 30
 let count = 30;
 //inseriamo nell'elemento html il contatore
 countdown.innerText = count;
+//prendo l'elemento input-group
+const inputGroup = document.getElementById("input-group");
+//prendo tutti gli elementi di input-group
+const inputs = inputGroup.querySelectorAll("input");
+//inizializzo array che conterrà i valori degli input
+let inputValues = [];
+//valore numerico che rappresenta il numero di elementi in comune tra i due array
+let numberScore=0;
 
 //inseriamo i figli di ul nell html
 for (let i = 0; i < randomNumbers.length; i++) {
@@ -18,13 +28,41 @@ for (let i = 0; i < randomNumbers.length; i++) {
 
 //Decrementiamo il contatore ogni secondo fino a 0 ed aggiorniamo il valore del countdown
 const intervalId = setInterval(function () {
-    if (count === 1) {
-        clearInterval(intervalId);
-    }
     count--;
+    if (count === 0) {
+        clearInterval(intervalId);
+        //se count è uguale a 0 aggiungiamo display none all'ul e togliamo display none ad answers-form
+        answersForm.classList.remove("d-none");
+        ul.classList.add("d-none");
+    }
+
     countdown.innerText = count;
 }, 1_000);
 
+
+
+answersForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    //scorro gli input ed inserisco i valori nell array
+    for(let i=0; i<inputs.length;i++){
+        inputValues.push(parseInt(inputs[i].value));
+    }
+    console.log(inputValues);
+
+    //utilizziamo la funzione che confronta due array e salviamo il risultato nella variabile numberScore
+    numberScore= twoArrayComparison(inputValues,randomNumbers);
+    console.log(numberScore);
+
+
+})
+
+
+
+
+
+
+
+//FUNZIONI:
 
 //funzione che genera un array di 5 numeri random da 1 a 50
 function fiveRandomNumbers() {
@@ -33,4 +71,15 @@ function fiveRandomNumbers() {
         array[i] = Math.floor(Math.random() * 50) + 1;
     }
     return array;
+}
+
+//funzione che confronta due array e restituisce il numero di elementi in comune
+function twoArrayComparison (arrayOne,arrayTwo) {
+    let count = 0;
+    for(let i=0; i<arrayOne.length;i++) {
+        if(arrayTwo.includes(arrayOne[i])) {
+            count++;
+        }
+    }
+    return count;
 }
